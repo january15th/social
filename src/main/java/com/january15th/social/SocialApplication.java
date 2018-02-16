fd87a89d2f38ddc65d406e1b11cbb1f912508299
january15th <reggiechapple@icloud.com>
2018-02-15 17:17:09 -0800

    first commit

---
+++ src/main/java/com/january15th/social/SocialApplication.java
@@ -0,0 +1,12 @@
+package com.january15th.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+}
